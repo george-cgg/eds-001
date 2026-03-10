2b40d78c5b2d284d58d48c8ad22622716fdb8c9b
chiriac <chiriac@adobe.com>
2026-03-10 15:54:21 +0200

    dev

--- blocks/sunstargum-get-nearby-stores/sunstargum-get-nearby-stores.js
+++ blocks/sunstargum-get-nearby-stores/sunstargum-get-nearby-stores.js
@@ -64,7 +64,7 @@ function createStoreCard(store) {
 
   const directionsBtn = document.createElement('a');
   directionsBtn.className = 'gum-store-btn';
-  directionsBtn.textContent = 'Get Directions';
+  directionsBtn.innerHTML = `<svg class="gum-store-btn-icon" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clip-rule="evenodd"/></svg> Get Directions`;
   directionsBtn.href = store.directionsUrl || `https://maps.google.com/?q=${encodeURIComponent(`${store.address}, ${store.city}, ${store.state} ${store.zip}`)}`;
   directionsBtn.target = '_blank';
   directionsBtn.rel = 'noopener noreferrer';
